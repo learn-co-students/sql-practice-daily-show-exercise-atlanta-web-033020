e47828e8216ea5cac4ffe02b02f74f25f8fcd679
github username <github email address>
2020-04-09 21:28:39 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,8 @@
 require 'bundler'
+require 'sqlite3'
+require_relative '../lib/queries'
+require_relative '../db/seed'
 Bundler.require
 
 # Setup a DB connection here
+DB = {:conn => SQLite3::Database.new("db/daily_show.db")}
